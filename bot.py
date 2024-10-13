ccef69f5fdec7f5b484d995a21f3b0f585b186c2
owh <ilia.covali@gmail.com>
2024-10-13 23:29:27 +0300

    added keyboard, language command

--- bot.py
+++ bot.py
@@ -1,20 +1,53 @@
 import asyncio
 import logging
+import os
 import sys
-from os import getenv
 
-from aiogram import Bot, Dispatcher, html
+from aiogram import Bot, Dispatcher, html, types
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
+from dotenv import load_dotenv
 
+# TODO: Implement aiogram_i18n
 
-TOKEN = getenv("TELEGRAM_BOT_TOKEN")
+load_dotenv() # TODO: Implement loading only of Telegram Bot Token
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
+    kb = [
+        [
+            types.KeyboardButton(text="Русский 🇷🇺"),
+            types.KeyboardButton(text="English 🇬🇧")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите язык",
+    )
+    await message.answer(f"Hello, {message.from_user.first_name}!\n\nChoose your language:", reply_markup=keyboard)
+
+@dp.message(Command("language"))
+async def commmand_change_language(message: Message) -> None:
+    kb = [
+        [
+            types.KeyboardButton(text="Русский 🇷🇺"),
+            types.KeyboardButton(text="English 🇬🇧")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите язык",
+    )
+    await message.answer(f"Choose your language:", reply_markup=keyboard)
+    
+@dp.message(Command("send"))
+async def command_send_handler(message: Message) -> None:
     await message.answer(f"Hello, {message.from_user.first_name}!")
 
 async def main() -> None:
